06732ced0878522fe21b1faf301c87b83bd1e82c
이영주 <yungju9982@naver.com>
2022-07-11 04:41:09 +0900

    #11 [feat] Home, Rules, Profile 프래그먼트 데이터바인딩

--- app/src/main/java/com/hous/hous_aos/ui/home/HomeFragment.kt
+++ app/src/main/java/com/hous/hous_aos/ui/home/HomeFragment.kt
@@ -1,7 +1,26 @@
 package com.hous.hous_aos.ui.home
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.hous.hous_aos.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
--- app/src/main/java/com/hous/hous_aos/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/hous/hous_aos/ui/profile/ProfileFragment.kt
@@ -1,7 +1,26 @@
 package com.hous.hous_aos.ui.profile
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.hous.hous_aos.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
+    private var _binding: FragmentProfileBinding? = null
+    private val binding get() = _binding!!
 
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentProfileBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
