a8d339bd3ca668eea9aa97e53b42f10b69bf781e
KWY0218 <wodrk1001@gmail.com>
2022-07-11 17:04:08 +0900

    #19 [feat] 클릭 시 알림 버튼 색 변경

--- app/src/main/java/com/hous/hous_aos/ui/newrules/NewRulesScreen.kt
+++ app/src/main/java/com/hous/hous_aos/ui/newrules/NewRulesScreen.kt
@@ -1,13 +1,20 @@
 package com.hous.hous_aos.ui.newrules
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -18,10 +25,17 @@ import com.hous.hous_aos.R
 
 @Composable
 fun NewRulesScreen() {
+    val isAlarm = remember { mutableStateOf(false) }
+    Column() {
+        Spacer(modifier = Modifier.size(50.dp))
+        NewRulesToolbar(isAlarm)
+    }
 }
 
 @Composable
-fun NewRulesToolbar() {
+fun NewRulesToolbar(
+    isAlarm: MutableState<Boolean>
+) {
     Row(
         modifier = Modifier.fillMaxWidth().padding(horizontal = 20.dp),
         horizontalArrangement = Arrangement.SpaceBetween
@@ -34,17 +48,24 @@ fun NewRulesToolbar() {
             text = stringResource(id = R.string.new_rules_title),
             fontStyle = FontStyle(R.style.B1)
         )
-        Image(
-            painter = painterResource(id = R.drawable.ic_alarmon),
-            contentDescription = ""
-        )
+        if (isAlarm.value) {
+            Image(
+                painter = painterResource(id = R.drawable.ic_alarmon),
+                contentDescription = "",
+                modifier = Modifier.clickable { isAlarm.value = !isAlarm.value }
+            )
+        } else {
+            Image(
+                painter = painterResource(id = R.drawable.ic_alarmoff),
+                contentDescription = "",
+                modifier = Modifier.clickable { isAlarm.value = !isAlarm.value }
+            )
+        }
     }
 }
 
 @Preview
 @Composable
 fun NewRulesPreview() {
-    Column() {
-        NewRulesToolbar()
-    }
+    NewRulesScreen()
 }
