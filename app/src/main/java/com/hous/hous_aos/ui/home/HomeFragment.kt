501f8b05094027c92d66c036032d806bd368a1e3
이영주 <yungju9982@naver.com>
2022-07-11 16:24:15 +0900

    #14 [feat] coming up 어댑터와 리사이클러뷰 연결

--- app/src/main/java/com/hous/hous_aos/ui/home/HomeFragment.kt
+++ app/src/main/java/com/hous/hous_aos/ui/home/HomeFragment.kt
@@ -5,22 +5,32 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.hous.hous_aos.R
 import com.hous.hous_aos.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
     private val binding get() = _binding!!
+    private lateinit var comingUpAdapter: ComingUpAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
+        initAdapter()
         return binding.root
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun initAdapter() {
+        comingUpAdapter = ComingUpAdapter()
+            )
+        )
+        comingUpAdapter.notifyDataSetChanged()
     }
 }
