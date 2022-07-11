f818925b5442afdd1d6ac10871a307d8e8d9b424
KWY0218 <wodrk1001@gmail.com>
2022-07-11 20:42:59 +0900

    #19 [ui] 규칙 이름 edit text 추가

--- app/src/main/java/com/hous/hous_aos/ui/newrules/NewRulesScreen.kt
+++ app/src/main/java/com/hous/hous_aos/ui/newrules/NewRulesScreen.kt
@@ -1,6 +1,7 @@
 package com.hous.hous_aos.ui.newrules
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -9,26 +10,55 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.hous.hous_aos.R
 
 @Composable
 fun NewRulesScreen() {
     val isAlarm = remember { mutableStateOf(false) }
-    Column() {
+    val text = remember { mutableStateOf("") }
+    Column(
+        modifier = Modifier
+            .background(colorResource(id = R.color.hous_blue_bg))
+            .padding(horizontal = 20.dp)
+    ) {
         Spacer(modifier = Modifier.size(50.dp))
         NewRulesToolbar(isAlarm)
+
+        Spacer(modifier = Modifier.size(27.dp))
+        Text(
+            text = "규칙이름",
+            fontStyle = FontStyle(R.style.B2),
+            color = colorResource(id = R.color.hous_blue)
+        )
+
+        Spacer(modifier = Modifier.size(8.dp))
+        NewRulesTextField(text, 10.dp)
+
+        Spacer(modifier = Modifier.size(16.dp))
+        Text(
+            text = "카테고리",
+            fontStyle = FontStyle(R.style.B2),
+            color = colorResource(id = R.color.hous_blue)
+        )
+
+        Spacer(modifier = Modifier.size(8.dp))
     }
 }
 
@@ -37,7 +67,8 @@ fun NewRulesToolbar(
     isAlarm: MutableState<Boolean>
 ) {
     Row(
-        modifier = Modifier.fillMaxWidth().padding(horizontal = 20.dp),
+        modifier = Modifier
+            .fillMaxWidth(),
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
         Image(
@@ -62,6 +93,29 @@ fun NewRulesToolbar(
             )
         }
     }
+}
+
+@Composable
+fun NewRulesTextField(
+    text: MutableState<String>,
+    radius: Dp
+) {
+    TextField(
+        modifier = Modifier
+            .fillMaxWidth()
+            .scale(scaleY = 0.5F, scaleX = 1F),
+        value = text.value,
+        onValueChange = {
+            text.value = it
+        },
+        shape = RoundedCornerShape(radius),
+        colors = TextFieldDefaults.textFieldColors(
+            backgroundColor = colorResource(id = R.color.white),
+            unfocusedIndicatorColor = colorResource(id = R.color.white),
+            focusedIndicatorColor = colorResource(id = R.color.white),
+            cursorColor = colorResource(id = R.color.white),
+        ),
+    )
 }
 
 @Preview
