0b92f62a47c452a396afb532a7bf82a5e9f65067
murjune <sis9221302@gmail.com>
2022-07-12 22:13:35 +0900

    #28 [feat] 상단 리사이클러뷰 adapter 추가

--- app/src/main/java/com/hous/hous_aos/ui/rules/adapter/CategoryOfRuleAdapter.kt
+++ app/src/main/java/com/hous/hous_aos/ui/rules/adapter/CategoryOfRuleAdapter.kt
@@ -1,51 +1,86 @@
 package com.hous.hous_aos.ui.rules.adapter
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.bumptech.glide.Glide
 import com.hous.hous_aos.data.entity.rules.CategoryOfRuleResponse
 import com.hous.hous_aos.databinding.ItemRulesRuleBinding
+import com.hous.hous_aos.ui.rules.ItemCategoryViewType
 
-class CategoryOfRuleAdapter(private val onCategoryClick: () -> Unit) :
-    ListAdapter<CategoryOfRuleResponse, CategoryOfRuleAdapter.CategoryOfRuleViewHolder>(
+class CategoryOfRuleAdapter(
+    private val onCategoryClick: () -> Unit,
+    private val onPlusClick: () -> Unit,
+    private val onChangeIsSelected: (Int) -> Unit,
+) :
+    ListAdapter<CategoryOfRuleResponse, RecyclerView.ViewHolder>(
         CategoryOfRuleDiffUtilCallback
     ) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CategoryOfRuleViewHolder {
-        return CategoryOfRuleViewHolder(
-            binding = ItemRulesRuleBinding.inflate(
-                LayoutInflater.from(parent.context),
-                parent,
-                false
-            ),
-            onCategoryClick = onCategoryClick
-        )
+    override fun getItemViewType(position: Int): Int {
+        return if (position == currentList.size - 1) {
+            ItemCategoryViewType.PLUS_VIEW_TYPE.index
+        } else {
+            ItemCategoryViewType.GENERAL_VIEW_TYPE.index
+        }
     }
 
-    override fun onBindViewHolder(holder: CategoryOfRuleViewHolder, position: Int) {
-        val book = currentList[position]
-        holder.onBind(book)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType) {
+            ItemCategoryViewType.PLUS_VIEW_TYPE.index ->
+                PlusViewHolder(
+                    onPlusClick,
+                    ItemRulesRuleBinding.inflate(
+                        LayoutInflater.from(parent.context),
+                        parent,
+                        false
+                    )
+                )
+            else -> CategoryOfRuleViewHolder(
+                onCategoryClick,
+                onChangeIsSelected,
+                ItemRulesRuleBinding.inflate(
+                    LayoutInflater.from(parent.context), parent, false
+                )
+            )
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val data = currentList[position]
+        when (holder) {
+            is CategoryOfRuleViewHolder -> holder.onBind(data)
+            is PlusViewHolder -> holder.onBind(data)
+        }
     }
 
     class CategoryOfRuleViewHolder(
         private val onCategoryClick: () -> Unit,
+        private val onChangeIsSelected: (Int) -> Unit,
         private val binding: ItemRulesRuleBinding
     ) : RecyclerView.ViewHolder(binding.root) {
 
         fun onBind(data: CategoryOfRuleResponse) {
             binding.data = data
-            Glide.with(binding.ivBear.context)
-                .load(data.icon)
-                .into(binding.ivBear)
-            Glide.with(binding.ivRulesBackground.context)
-                .load(data.backGround)
-                .into(binding.ivRulesBackground)
+            binding.clRuleItem.setOnClickListener {
+                onCategoryClick()
+                onChangeIsSelected(adapterPosition)
+            }
+        }
+    }
 
-            binding.root.setOnClickListener {
-                onCategoryClick
+    class PlusViewHolder(
+        private val onPlusClick: () -> Unit,
+        private val binding: ItemRulesRuleBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun onBind(data: CategoryOfRuleResponse) {
+            binding.data = data
+            binding.ivRuleIcon.visibility = View.INVISIBLE
+            binding.clRuleItem.setOnClickListener {
+                onPlusClick()
             }
         }
     }
