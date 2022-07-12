8f8584ab8e312374e2d11f33520deb769b034428
KWY0218 <wodrk1001@gmail.com>
2022-07-12 17:01:27 +0900

    #34 [feat] api 별 어노테이션 생성

---
+++ app/src/main/java/com/hous/hous_aos/di/NetworkAnnotation.kt
@@ -0,0 +1,15 @@
+package com.hous.hous_aos.di
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class HOME
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class RULE
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class PROFILE
