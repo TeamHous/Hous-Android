1404b74fc29b30df5cefbc2f61a2e7b36a0536f5
이빵주 <yungju9982@naver.com>
2022-07-14 01:14:19 +0900

    #33 [feat] profile 어댑터 멀티뷰타입으로 변경

--- app/src/main/java/com/hous/hous_aos/ui/home/ProfileAdapter.kt
+++ app/src/main/java/com/hous/hous_aos/ui/home/ProfileAdapter.kt
@@ -2,33 +2,83 @@ package com.hous.hous_aos.ui.home
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.hous.hous_aos.databinding.ItemHomeProfileBinding
+import com.hous.hous_aos.databinding.ItemHomeProfileCopyBinding
 
-class ProfileAdapter : RecyclerView.Adapter<ProfileAdapter.ProfileViewHolder>() {
-    val profileList = mutableListOf<ProfileData>(ProfileData("이영주"))
+class ProfileAdapter() :
+    ListAdapter<ProfileData, RecyclerView.ViewHolder>(profileDiffUtil) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProfileViewHolder {
-        val binding =
-            ItemHomeProfileBinding.inflate(
-                LayoutInflater.from(parent.context),
-                parent,
-                false
-            )
-        return ProfileViewHolder(binding)
+    private lateinit var itemHomeProfileBinding: ItemHomeProfileBinding
+    private lateinit var itemHomeProfileLastBinding: ItemHomeProfileCopyBinding
+
+    override fun getItemViewType(position: Int): Int {
+        return when (position) {
+            currentList.size - 1 -> COPY_ITEM
+            else -> PROFILE_ITEM
+        }
     }
 
-    override fun onBindViewHolder(holder: ProfileViewHolder, position: Int) {
-        holder.onBind(profileList[position])
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType) {
+            PROFILE_ITEM -> {
+                itemHomeProfileBinding =
+                    ItemHomeProfileBinding.inflate(
+                        LayoutInflater.from(parent.context),
+                        parent,
+                        false
+                    )
+                ProfileViewHolder(itemHomeProfileBinding)
+            }
+            COPY_ITEM -> {
+                itemHomeProfileLastBinding =
+                    ItemHomeProfileCopyBinding.inflate(
+                        LayoutInflater.from(parent.context),
+                        parent,
+                        false
+                    )
+                CopyViewHolder(itemHomeProfileLastBinding)
+            }
+            else -> {
+                throw RuntimeException("알 수 없는 viewType error")
+            }
+        }
     }
 
-    override fun getItemCount(): Int = profileList.size
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val data = currentList[position]
+        when (holder) {
+            is ProfileViewHolder -> holder.onBind(data)
+            is CopyViewHolder -> holder.onBind(data)
+        }
+    }
 
-    class ProfileViewHolder(
-        private val binding: ItemHomeProfileBinding
-    ) : RecyclerView.ViewHolder(binding.root) {
+    class ProfileViewHolder(val binding: ItemHomeProfileBinding) :
+        RecyclerView.ViewHolder(binding.root) {
         fun onBind(data: ProfileData) {
             binding.profileData = data
         }
+    }
+
+    class CopyViewHolder(val binding: ItemHomeProfileCopyBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun onBind(data: ProfileData) {
+            binding.profileData = data
+        }
+    }
+
+    companion object {
+        private val profileDiffUtil = object : DiffUtil.ItemCallback<ProfileData>() {
+            override fun areItemsTheSame(oldItem: ProfileData, newItem: ProfileData): Boolean =
+                oldItem.name == newItem.name
+
+            override fun areContentsTheSame(oldItem: ProfileData, newItem: ProfileData): Boolean =
+                oldItem == newItem
+        }
+
+        private const val PROFILE_ITEM = 0
+        private const val COPY_ITEM = 1
     }
 }
