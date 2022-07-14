a494456f514ffa7737b6b6b431da1f5f4aa9f7b2
KWY0218 <wodrk1001@gmail.com>
2022-07-15 01:33:11 +0900

    #31 [feat] edit text 가상 키보드 숨기는 기능 구현

--- app/src/main/java/com/hous/hous_aos/ui/newrules/NewRulesScreen.kt
+++ app/src/main/java/com/hous/hous_aos/ui/newrules/NewRulesScreen.kt
@@ -3,6 +3,7 @@ package com.hous.hous_aos.ui.newrules
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -21,6 +22,8 @@ import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.DropdownMenu
 import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.Text
@@ -35,14 +38,18 @@ import androidx.compose.ui.Alignment.Companion.BottomCenter
 import androidx.compose.ui.Alignment.Companion.CenterEnd
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.FocusManager
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
@@ -101,6 +108,7 @@ fun NewRulesScreen() {
             )
         )
     }
+    val focusManager = LocalFocusManager.current
     isRuleAddButton.value = uiState.value.ruleName.isNotEmpty() &&
         uiState.value.categoryName.isNotEmpty() &&
         (checkBoxState.value == State.SELECT || IsAdd(test))
@@ -110,6 +118,11 @@ fun NewRulesScreen() {
             .fillMaxSize()
             .background(colorResource(id = R.color.hous_blue_bg))
             .padding(horizontal = 20.dp)
+            .pointerInput(Unit) {
+                detectTapGestures(onTap = {
+                    focusManager.clearFocus()
+                })
+            }
     ) {
         LazyColumn(
             modifier = Modifier
@@ -128,7 +141,7 @@ fun NewRulesScreen() {
                 )
                 Spacer(modifier = Modifier.size(8.dp))
 
-                NewRulesTextField(uiState)
+                NewRulesTextField(uiState, focusManager)
                 Spacer(modifier = Modifier.size(16.dp))
 
                 Text(
@@ -243,9 +256,11 @@ fun NewRulesToolbar(
 
 @Composable
 fun NewRulesTextField(
-    uiState: MutableState<NewRulesUiState>
+    uiState: MutableState<NewRulesUiState>,
+    focusManager: FocusManager
 ) {
     val maxChar = 16
+
     BasicTextField(
         value = uiState.value.ruleName,
         onValueChange = {
@@ -263,7 +278,9 @@ fun NewRulesTextField(
         ),
         maxLines = 1,
         singleLine = true,
-        cursorBrush = SolidColor(colorResource(id = R.color.hous_blue))
+        cursorBrush = SolidColor(colorResource(id = R.color.hous_blue)),
+        keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Done),
+        keyboardActions = KeyboardActions(onDone = { focusManager.clearFocus() })
     )
 }
 
