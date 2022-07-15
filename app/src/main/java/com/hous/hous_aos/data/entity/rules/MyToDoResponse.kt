11daa3c066710462e0f01df3727461d7966372ea
murjune <sis9221302@gmail.com>
2022-07-16 05:41:42 +0900

    #52 [feat] MyToDo Data클래스 만들기

---
+++ app/src/main/java/com/hous/hous_aos/data/entity/rules/MyToDoResponse.kt
@@ -0,0 +1,12 @@
+package com.hous.hous_aos.data.entity.rules
+
+data class MyToDoResponse(
+    val _ids: String,
+    val categoryIcon: String,
+    val ruleName: String,
+    val isChecked: Boolean
+)
+//            "_id": "62cef1fd5742e5b90c586d7d",
+//            "categoryIcon": "CLEAN",
+//            "ruleName": " 임시 담당자 설정 테스트",
+//            "isChecked": false
