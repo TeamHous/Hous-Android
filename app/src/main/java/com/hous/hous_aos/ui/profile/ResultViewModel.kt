775a89aac2e605b4527ff48e2a7312604d0b24a3
이빵주 <yungju9982@naver.com>
2022-07-18 10:02:15 +0900

    #68 [feat] result 뷰모델 생성

---
+++ app/src/main/java/com/hous/hous_aos/ui/profile/ResultViewModel.kt
@@ -0,0 +1,8 @@
+package com.hous.hous_aos.ui.profile
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.hous.hous_aos.R
+
+class ResultViewModel : ViewModel() {
+    private val _resultData = MutableLiveData(
