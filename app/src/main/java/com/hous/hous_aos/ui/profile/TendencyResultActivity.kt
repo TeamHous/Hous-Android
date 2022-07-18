3f070ac30a8657d0339fe92eb61728048b613ba9
이빵주 <yungju9982@naver.com>
2022-07-18 10:05:31 +0900

    #68 [feat] result 액티비티에 viewmodel 연결

--- app/src/main/java/com/hous/hous_aos/ui/profile/TendencyResultActivity.kt
+++ app/src/main/java/com/hous/hous_aos/ui/profile/TendencyResultActivity.kt
@@ -1,13 +1,18 @@
 package com.hous.hous_aos.ui.profile
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.hous.hous_aos.R
 import com.hous.hous_aos.databinding.ActivityTendencyResultBinding
+import com.hous.hous_aos.util.setDrawable
 
 class TendencyResultActivity : AppCompatActivity() {
     private lateinit var binding: ActivityTendencyResultBinding
+    val viewModel: ResultViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_tendency_result)
