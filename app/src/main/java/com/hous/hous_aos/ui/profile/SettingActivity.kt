e583e7fdebfca6f3493494aa3076c9187b72ddff
KWY0218 <wodrk1001@gmail.com>
2022-07-18 19:14:14 +0900

    #74 [chore] drawable 추가

---
+++ app/src/main/java/com/hous/hous_aos/ui/profile/SettingActivity.kt
@@ -0,0 +1,12 @@
+package com.hous.hous_aos.ui.profile
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.hous.hous_aos.R
+
+class SettingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting)
+    }
+}
