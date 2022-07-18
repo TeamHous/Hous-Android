11f098af1cc6effebdbb404562212416cdc99bd7
이빵주 <yungju9982@naver.com>
2022-07-18 10:05:20 +0900

    #68 [chore] 불필요한 코드 제거

--- app/src/main/java/com/hous/hous_aos/ui/profile/ResultViewModel.kt
+++ app/src/main/java/com/hous/hous_aos/ui/profile/ResultViewModel.kt
@@ -2,7 +2,6 @@ package com.hous.hous_aos.ui.profile
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.hous.hous_aos.R
 
 class ResultViewModel : ViewModel() {
     private val _resultData = MutableLiveData(
