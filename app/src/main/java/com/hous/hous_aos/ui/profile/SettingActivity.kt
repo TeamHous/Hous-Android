a7064a3e15a55636c1ca8a1e8a68bea13a0aa576
KWY0218 <wodrk1001@gmail.com>
2022-07-18 20:27:58 +0900

    #74 [ui] 환경설정 뷰 구현

--- app/src/main/java/com/hous/hous_aos/ui/profile/SettingActivity.kt
+++ app/src/main/java/com/hous/hous_aos/ui/profile/SettingActivity.kt
@@ -2,11 +2,14 @@ package com.hous.hous_aos.ui.profile
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.hous.hous_aos.R
+import com.hous.hous_aos.databinding.ActivitySettingBinding
 
 class SettingActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySettingBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_setting)
+        binding = ActivitySettingBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.ivSettingAlarm.setOnClickListener { it.isSelected = !it.isSelected }
     }
 }
