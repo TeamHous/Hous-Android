0adf966bc9f04de8ce6e911f3f7fe7aab34e44cb
이빵주 <yungju9982@naver.com>
2022-07-19 22:23:08 +0900

    #44 [feat] roommate card 액티비티 데이터바인딩

--- app/src/main/java/com/hous/hous_aos/ui/home/RoommateCardActivity.kt
+++ app/src/main/java/com/hous/hous_aos/ui/home/RoommateCardActivity.kt
@@ -1,12 +1,15 @@
 package com.hous.hous_aos.ui.home
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.hous.hous_aos.R
+import com.hous.hous_aos.databinding.ActivityRoommateCardBinding
 
 class RoommateCardActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRoommateCardBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_roommate_card)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_roommate_card)
     }
 }
