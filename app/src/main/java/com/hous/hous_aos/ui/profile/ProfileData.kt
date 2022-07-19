23de4c18b505307178a6b78e5599ed9d81e07ac5
이빵주 <yungju9982@naver.com>
2022-07-19 23:05:26 +0900

    #82 [feat] profile data 수정

--- app/src/main/java/com/hous/hous_aos/ui/profile/ProfileData.kt
+++ app/src/main/java/com/hous/hous_aos/ui/profile/ProfileData.kt
@@ -1,7 +1,12 @@
 package com.hous.hous_aos.ui.profile
 
 data class ProfileData(
-    val name: String,
+    val userName: String,
     val job: String,
-    val introduce: String,
+    val introduction: String,
+    val hashTag: List<String>,
+    val typeName: String,
+    val typeColor: String,
+    val typeScore: List<Int>,
+    val notificationState: Boolean
 )
