e08fbc435f3c7c553c42cfcb5f7ce29ad31058e8
이빵주 <yungju9982@naver.com>
2022-07-19 22:20:59 +0900

    #44 [feat] roommate card 액티비티 생성

---
+++ app/src/main/java/com/hous/hous_aos/ui/home/RoommateCardActivity.kt
@@ -0,0 +1,12 @@
+package com.hous.hous_aos.ui.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.hous.hous_aos.R
+
+class RoommateCardActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_roommate_card)
+    }
+}
