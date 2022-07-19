95bc6685b0f2a2217153f81a3bd29dca36f62d13
murjune <sis9221302@gmail.com>
2022-07-20 00:26:38 +0900

    #78 [feat] EventIcon 이노옴!!클래스 구현

---
+++ app/src/main/java/com/hous/hous_aos/ui/home/EventIcon.kt
@@ -0,0 +1,8 @@
+package com.hous.hous_aos.ui.home
+
+import com.hous.hous_aos.R
+
+enum class EventIcon(val drawableRes: Int) {
+    FIRST(R.drawable.ic_evemt_party_m), SECOND(R.drawable.ic_event_pancake_m),
+    THIRD(R.drawable.ic_event_beer_m), FOURTH(R.drawable.ic_event_coffee_m)
+}
