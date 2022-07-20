808b4a034b5c0ad68177606fd3ab82630ee1ea6e
이빵주 <yungju9982@naver.com>
2022-07-21 00:09:24 +0900

    #44 [chore] import 추가

--- app/src/main/java/com/hous/hous_aos/ui/home/HomeFragment.kt
+++ app/src/main/java/com/hous/hous_aos/ui/home/HomeFragment.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.GridLayoutManager
 import com.hous.hous_aos.databinding.FragmentHomeBinding
 import com.hous.hous_aos.ui.home.adapter.EventAdapter
