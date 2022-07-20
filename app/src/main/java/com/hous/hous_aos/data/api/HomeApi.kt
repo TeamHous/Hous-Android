5134a17ff2fd8dfcc82919e78115c1a0f5e99733
이빵주 <yungju9982@naver.com>
2022-07-21 01:53:46 +0900

    #92 [feat] HomeApi 생성

--- app/src/main/java/com/hous/hous_aos/data/api/HomeApi.kt
+++ app/src/main/java/com/hous/hous_aos/data/api/HomeApi.kt
@@ -1,3 +1,13 @@
 package com.hous.hous_aos.data.api
 
-interface HomeApi
+import com.hous.hous_aos.data.model.WrapperClass
+import com.hous.hous_aos.data.model.response.HomeResponse
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+interface HomeApi {
+    @GET("room/{roomId}/home")
+    suspend fun getHomeList(
+        @Path("roomId") roomId: String
+    ): WrapperClass<HomeResponse>
+}
