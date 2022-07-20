e1d0f5f4dfd085b66d01c708939675faea2978f7
이빵주 <yungju9982@naver.com>
2022-07-21 01:54:54 +0900

    #92 [feat] RemoteHomeDataSource 생성

--- app/src/main/java/com/hous/hous_aos/data/source/remote/RemoteHomeDataSource.kt
+++ app/src/main/java/com/hous/hous_aos/data/source/remote/RemoteHomeDataSource.kt
@@ -1,3 +1,9 @@
 package com.hous.hous_aos.data.source.remote
 
-interface RemoteHomeDataSource
+import com.hous.hous_aos.data.model.WrapperClass
+import com.hous.hous_aos.data.model.response.HomeResponse
+import retrofit2.http.Path
+
+interface RemoteHomeDataSource{
+    suspend fun getHomeList(roomId: String): WrapperClass<HomeResponse>
+}
