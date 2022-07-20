da1de781b977344beaf43a2615c93d7346798616
이빵주 <yungju9982@naver.com>
2022-07-20 20:43:58 +0900

    #44 [feat] import 추가

--- app/src/main/java/com/hous/hous_aos/ui/home/RoommateCardActivity.kt
+++ app/src/main/java/com/hous/hous_aos/ui/home/RoommateCardActivity.kt
@@ -1,10 +1,18 @@
 package com.hous.hous_aos.ui.home
 
+import android.content.Intent
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.view.View
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.hous.hous_aos.R
 import com.hous.hous_aos.databinding.ActivityRoommateCardBinding
+import com.hous.hous_aos.ui.profile.ProfileViewModel
+import com.hous.hous_aos.ui.profile.TendencyResultActivity
+import com.hous.hous_aos.util.setDrawable
 
 class RoommateCardActivity : AppCompatActivity() {
     private lateinit var binding: ActivityRoommateCardBinding
