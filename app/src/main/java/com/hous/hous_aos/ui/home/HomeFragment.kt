c2a6ac33bb89458efdb57193c883c4450e872a0f
이빵주 <yungju9982@naver.com>
2022-07-21 11:17:37 +0900

    #108 [chore] import

--- app/src/main/java/com/hous/hous_aos/ui/home/HomeFragment.kt
+++ app/src/main/java/com/hous/hous_aos/ui/home/HomeFragment.kt
@@ -2,6 +2,7 @@ package com.hous.hous_aos.ui.home
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
