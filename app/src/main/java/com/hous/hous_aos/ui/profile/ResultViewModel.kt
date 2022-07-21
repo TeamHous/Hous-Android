6f55dfb5544b61b977ed6818e130b6ea35cea600
이빵주 <yungju9982@naver.com>
2022-07-21 20:22:03 +0900

    #108 [feat] 코루틴 생성

--- app/src/main/java/com/hous/hous_aos/ui/profile/ResultViewModel.kt
+++ app/src/main/java/com/hous/hous_aos/ui/profile/ResultViewModel.kt
@@ -1,8 +1,33 @@
 package com.hous.hous_aos.ui.profile
 
+import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.hous.hous_aos.data.entity.ResultData
+import com.hous.hous_aos.data.repository.ProfileRepository
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
+class ResultViewModel @Inject constructor(
+    private val profileRepository: ProfileRepository
+) : ViewModel() {
+    private val _resultData = MutableLiveData<ResultData>()
     val resultData get() = _resultData
+
+    private val _typeId = MutableLiveData<String>()
+    val typeId get() = _typeId
+
+    fun getResult() {
+        viewModelScope.launch {
+            profileRepository.getMyResult(typeId = typeId.value!!)
+                .onSuccess {
+                    Log.d("ResultViewModel", "data : ${it.data}")
+                    _resultData.value = it.data!!
+                }
+                .onFailure {
+                    Log.d("ResultViewModel", "data : ${it.message}")
+                }
+        }
+    }
 }
