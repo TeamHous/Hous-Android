d56602200ac7e0bfc964ae945859a7e220a62043
이빵주 <yungju9982@naver.com>
2022-07-21 11:20:13 +0900

    #108 [feat] repository implement 생성

--- app/src/main/java/com/hous/hous_aos/data/repository/HomeRepositoryImpl.kt
+++ app/src/main/java/com/hous/hous_aos/data/repository/HomeRepositoryImpl.kt
@@ -1,5 +1,6 @@
 package com.hous.hous_aos.data.repository
 
+import com.hous.hous_aos.data.entity.Homie
 import com.hous.hous_aos.data.model.WrapperClass
 import com.hous.hous_aos.data.model.response.HomeResponse
 import com.hous.hous_aos.data.source.remote.RemoteHomeDataSource
@@ -7,8 +8,11 @@ import javax.inject.Inject
 
 class HomeRepositoryImpl @Inject constructor(
     private val homeDataSource: RemoteHomeDataSource
-) :HomeRepository {
+) : HomeRepository {
     override suspend fun getHomeList(roomId: String): Result<WrapperClass<HomeResponse>> {
         return runCatching { homeDataSource.getHomeList(roomId) }
+    }
+    override suspend fun getHomieList(homieId: String): Result<WrapperClass<Homie>> {
+        return runCatching { homeDataSource.getHomieList(homieId) }
     }
 }
