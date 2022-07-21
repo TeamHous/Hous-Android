b22f4a070c07792ed985e3e0632e6f4bb06a7750
이빵주 <yungju9982@naver.com>
2022-07-21 19:51:39 +0900

    #108 [feat] remote data source 생성

--- app/src/main/java/com/hous/hous_aos/data/source/remote/RemoteProfileDataSource.kt
+++ app/src/main/java/com/hous/hous_aos/data/source/remote/RemoteProfileDataSource.kt
@@ -3,10 +3,12 @@ package com.hous.hous_aos.data.source.remote
 import com.hous.hous_aos.data.entity.Homie
 import com.hous.hous_aos.data.model.WrapperClass
 import com.hous.hous_aos.data.model.request.PutTestResultRequest
+import com.hous.hous_aos.data.model.response.TestResultResponse
 import com.hous.hous_aos.data.model.response.TypeTestResponse
 
 interface RemoteProfileDataSource {
     suspend fun getUserProfile(): WrapperClass<Homie>
     suspend fun putTestResult(typeScore: PutTestResultRequest): WrapperClass<Any>
     suspend fun getTypeTestList(): WrapperClass<TypeTestResponse>
+    suspend fun getMyResult(typeId: Int): WrapperClass<TestResultResponse>
 }
