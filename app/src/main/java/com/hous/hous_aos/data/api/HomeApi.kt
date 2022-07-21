dd5c194d183580fbec94d3a417e3cb2574d04365
이빵주 <yungju9982@naver.com>
2022-07-21 11:18:15 +0900

    #108 [feat] api에 getHomeList 생성

--- app/src/main/java/com/hous/hous_aos/data/api/HomeApi.kt
+++ app/src/main/java/com/hous/hous_aos/data/api/HomeApi.kt
@@ -1,5 +1,6 @@
 package com.hous.hous_aos.data.api
 
+import com.hous.hous_aos.data.entity.Homie
 import com.hous.hous_aos.data.model.WrapperClass
 import com.hous.hous_aos.data.model.response.HomeResponse
 import retrofit2.http.GET
@@ -10,4 +11,9 @@ interface HomeApi {
     suspend fun getHomeList(
         @Path("roomId") roomId: String
     ): WrapperClass<HomeResponse>
+
+    @GET("user/{homieId}")
+    suspend fun getHomieList(
+        @Path("homieId") homieId: String
+    ): WrapperClass<Homie>
 }
