3a1e9078ad82dbca74ebe43ddd5cc67c410b031a
이빵주 <yungju9982@naver.com>
2022-07-21 11:19:58 +0900

    #108 [feat] repository 생성

--- app/src/main/java/com/hous/hous_aos/data/repository/HomeRepository.kt
+++ app/src/main/java/com/hous/hous_aos/data/repository/HomeRepository.kt
@@ -1,8 +1,10 @@
 package com.hous.hous_aos.data.repository
 
+import com.hous.hous_aos.data.entity.Homie
 import com.hous.hous_aos.data.model.WrapperClass
 import com.hous.hous_aos.data.model.response.HomeResponse
 
 interface HomeRepository {
     suspend fun getHomeList(roomId: String): Result<WrapperClass<HomeResponse>>
+    suspend fun getHomieList(homieId: String): Result<WrapperClass<Homie>>
 }
