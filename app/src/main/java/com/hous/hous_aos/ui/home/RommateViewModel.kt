26c237ea86fd9e9ca35a4b9d1be1c7d645700f65
이빵주 <yungju9982@naver.com>
2022-07-21 11:11:16 +0900

    #108 [feat] 룸메이트 뷰모델 생성

---
+++ app/src/main/java/com/hous/hous_aos/ui/home/RommateViewModel.kt
@@ -0,0 +1,12 @@
+package com.hous.hous_aos.ui.home
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.hous.hous_aos.data.entity.Homie
+import com.hous.hous_aos.data.repository.HomeRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
