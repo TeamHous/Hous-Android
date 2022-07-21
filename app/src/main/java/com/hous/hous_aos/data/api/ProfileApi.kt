eb9a518c0aab4a0391066be4c090e802f0b4adac
이빵주 <yungju9982@naver.com>
2022-07-21 19:31:54 +0900

    #108 [feat] 프로필api에 getMyResult  추가

--- app/src/main/java/com/hous/hous_aos/data/api/ProfileApi.kt
+++ app/src/main/java/com/hous/hous_aos/data/api/ProfileApi.kt
@@ -4,9 +4,11 @@ import com.hous.hous_aos.data.entity.Homie
 import com.hous.hous_aos.data.model.WrapperClass
 import com.hous.hous_aos.data.model.request.PutTestResultRequest
 import com.hous.hous_aos.data.model.response.TypeTestResponse
+import com.hous.hous_aos.data.entity.ResultData
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.PUT
+import retrofit2.http.Path
 
 interface ProfileApi {
     @GET("user/profile")
@@ -19,4 +21,9 @@ interface ProfileApi {
 
     @GET("type/test")
     suspend fun getTypeTestList(): WrapperClass<TypeTestResponse>
+
+    @GET("user/me/type")
+    suspend fun getMyResult(
+        @Path("typeId") typeId: String
+    ): WrapperClass<ResultData>
 }
