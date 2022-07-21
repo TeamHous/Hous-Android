7e0c8d12e8d9995cbffcfe1d09c80fd8cb4534a2
murjune <sis9221302@gmail.com>
2022-07-21 22:36:37 +0900

    #127 [feat] RulesTable api 연결

---
+++ app/src/main/java/com/hous/hous_aos/data/model/response/RulesTableResponse.kt
@@ -0,0 +1,8 @@
+package com.hous.hous_aos.data.model.response
+
+import com.hous.hous_aos.data.entity.Rule
+
+data class RulesTableResponse(
+    val keyRules: List<Rule>,
+    val rules: List<Rule>
+)
