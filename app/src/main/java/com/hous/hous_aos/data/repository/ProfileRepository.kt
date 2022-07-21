85ccca4c3d0cddf24466b262a96b45d19259f435
이빵주 <yungju9982@naver.com>
2022-07-21 20:13:59 +0900

    #108 [feat] repository 생성

--- app/src/main/java/com/hous/hous_aos/data/repository/ProfileRepository.kt
+++ app/src/main/java/com/hous/hous_aos/data/repository/ProfileRepository.kt
@@ -1,6 +1,7 @@
 package com.hous.hous_aos.data.repository
 
 import com.hous.hous_aos.data.entity.Homie
+import com.hous.hous_aos.data.entity.ResultData
 import com.hous.hous_aos.data.model.WrapperClass
 import com.hous.hous_aos.data.model.request.PutTestResultRequest
 import com.hous.hous_aos.data.model.response.TypeTestResponse
@@ -9,4 +10,5 @@ interface ProfileRepository {
     suspend fun getUserProfile(): Result<WrapperClass<Homie>>
     suspend fun putTestResult(typeScore: PutTestResultRequest): Result<WrapperClass<Any>>
     suspend fun getTypeTestList(): Result<WrapperClass<TypeTestResponse>>
+    suspend fun getMyResult(typeId: String): Result<WrapperClass<ResultData>>
 }
