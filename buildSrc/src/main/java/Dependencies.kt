66d73294199c117c89e845f7fdccda5297f71422
KWY0218 <wodrk1001@gmail.com>
2022-08-01 13:37:38 +0900

    #172 [chore] dependency, 버전 추가

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,1 +1,65 @@
-object Dependencies
+object KotlinDependencies {
+    const val kotlin = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlinVersion}"
+    const val coroutine =
+        "org.jetbrains.kotlinx:kotlinx-coroutines-android:${Versions.coroutineVersion}"
+}
+
+object AndroidXDependencies {
+    const val appCompat = "androidx.appcompat:appcompat:${Versions.appCompatVersion}"
+    const val constraintLayout =
+        "androidx.constraintlayout:constraintlayout:${Versions.constraintLayoutVersion}"
+    const val hilt = "com.google.dagger:hilt-android:${Versions.hiltVersion}"
+    const val recyclerview = "androidx.recyclerview:recyclerview:${Versions.recyclerviewVersion}"
+    const val composeActivity =
+        "androidx.activity:activity-compose:${Versions.composeActivityVersion}"
+    const val composeAnimation = "androidx.compose.animation:animation:${Versions.composeVersion}"
+    const val composeUi = "androidx.compose.ui:ui-tooling:${Versions.composeVersion}"
+    const val composeViewModel =
+        "androidx.lifecycle:lifecycle-viewmodel-compose:${Versions.composeViewModelVersion}"
+    const val composeTheme =
+        "com.google.accompanist:accompanist-appcompat-theme:${Versions.composeThemeVersion}"
+}
+
+object KTXDependencies {
+    const val coreKTX = "androidx.core:core-ktx:${Versions.coreKtxVersion}"
+    const val activityKTX = "androidx.activity:activity-ktx:${Versions.activityKTXVersion}"
+    const val lifecycleKTX =
+        "androidx.lifecycle:lifecycle-runtime-ktx:${Versions.lifecycleKTXVersion}"
+    const val fragmentKTX = "androidx.fragment:fragment-ktx:${Versions.fragmentKTXVersion}"
+}
+
+object TestDependencies {
+    const val jUnit = "junit:junit:${Versions.jUnitVersion}"
+    const val androidTest = "androidx.test.ext:junit:${Versions.androidTestVersion}"
+    const val espresso = "androidx.test.espresso:espresso-core:${Versions.espressoVersion}"
+    const val composeTest = "androidx.compose.ui:ui-test-junit4:${Versions.composeVersion}"
+}
+
+object MaterialDependencies {
+    const val material = "com.google.android.material:material:${Versions.materialVersion}"
+    const val composeMaterial = "androidx.compose.material:material:${Versions.composeVersion}"
+    const val composeAdapter =
+        "com.google.android.material:compose-theme-adapter:${Versions.composeAdapterVersion}"
+}
+
+object KaptDependencies {
+    const val hiltKapt = "com.google.dagger:hilt-android-compiler:${Versions.hiltVersion}"
+    const val glideKapt = "com.github.bumptech.glide:compiler:${Versions.glideVersion}"
+}
+
+object ThirdPartyDependencies {
+    const val coil = "io.coil-kt:coil:${Versions.coilVersion}"
+    const val interceptor =
+        "com.squareup.okhttp3:logging-interceptor:${Versions.interceptorVersion}"
+    const val gson = "com.google.code.gson:gson:${Versions.gsonVersion}"
+    const val retrofit2 = "com.squareup.retrofit2:retrofit:${Versions.retrofit2Version}"
+    const val retrofit2Converter =
+        "com.squareup.retrofit2:converter-gson:${Versions.retrofit2Version}"
+    const val timber = "com.jakewharton.timber:timber:${Versions.timberVersion}"
+    const val lottie = "com.airbnb.android:lottie:${Versions.lottieVersion}"
+    const val glide = "com.github.bumptech.glide:glide:${Versions.glideVersion}"
+}
+
+object ClassPathPlugins {
+    const val hilt = "com.google.dagger:hilt-android-gradle-plugin:${Versions.hiltVersion}"
+}
--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -1,1 +1,31 @@
-object Versions
+object Versions {
+    const val coreKtxVersion = "1.8.0"
+    const val appCompatVersion = "1.4.2"
+    const val materialVersion = "1.6.1"
+    const val constraintLayoutVersion = "2.1.4"
+    const val jUnitVersion = "4.13.2"
+    const val androidTestVersion = "1.1.3"
+    const val espressoVersion = "3.4.0"
+    const val coilVersion = "2.1.0"
+    const val interceptorVersion = "4.10.0"
+    const val gsonVersion = "2.9.0"
+    const val timberVersion = "5.0.1"
+    const val recyclerviewVersion = "1.2.1"
+    const val coroutineVersion = "1.6.3"
+    const val hiltVersion = "2.38.1"
+    const val retrofit2Version = "2.9.0"
+    const val lottieVersion = "5.2.0"
+    const val composeActivityVersion = "1.5.1"
+    const val composeVersion = "1.1.1"
+    const val composeViewModelVersion = "2.5.0"
+    const val composeAdapterVersion = "1.1.14"
+    const val composeThemeVersion = "0.16.0"
+    const val glideVersion = "4.13.2"
+
+    const val activityKTXVersion = "1.5.1"
+    const val lifecycleKTXVersion = "2.6.0-alpha01"
+    const val fragmentKTXVersion = "1.5.0"
+
+    const val gradleVersion = "7.1.1"
+    const val kotlinVersion = "1.6.20"
+}
