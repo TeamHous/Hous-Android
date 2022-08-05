b2d28108c2d8cae1572ffa6c260e08647d106be1
KWY0218 <wodrk1001@gmail.com>
2022-08-05 09:36:38 +0900

    #173 [add] dependency, 버전 추가

--- buildSrc/src/main/java/Constants.kt
+++ buildSrc/src/main/java/Constants.kt
@@ -1,1 +1,8 @@
-class Constants
+object Constants {
+    const val packageName = "com.hous.hous_aos"
+    const val compileSdk = 32
+    const val minSdk = 26
+    const val targetSdk = 32
+    const val versionCode = 1
+    const val versionName = "1.0"
+}
