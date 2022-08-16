8ee66237b445e81c9c1bae0876c1cb83ea996485
murjune <sis9221302@gmail.com>
2022-08-17 04:55:37 +0900

    #178 [feat] safeLet 확장함수 추가

---
+++ app/src/main/java/com/hous/hous_aos/util/SafeLet.kt
@@ -0,0 +1,7 @@
+package com.hous.hous_aos.util
+
+/**
+ * 변수 2가지 이상 null 체크할 때 사용 */
+inline fun <T1 : Any, T2 : Any, R : Any> safeLet(p1: T1?, p2: T2?, block: (T1, T2) -> R?): R? {
+    return if (p1 != null && p2 != null) block(p1, p2) else null
+}
